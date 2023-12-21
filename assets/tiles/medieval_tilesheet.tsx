301ec3d5630bd64788594dbddaaf958dc1d8dbe6
jeaguil <18241ja@gmail.com>
2023-12-20 21:47:54 -0800

    Tiled tmx file

---
+++ assets/tiles/medieval_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="medieval_tilesheet" tilewidth="32" tileheight="32" tilecount="1210" columns="55">
+ <image source="Git/forgotten-fields/assets/tiles/medieval_tilesheet.png" width="1760" height="704"/>
+</tileset>
